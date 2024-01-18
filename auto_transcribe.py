9e68a34dcfe2c9ceb6d6aa9aa860c6a5ed33c3ce
YYuX-1145 <138500330+YYuX-1145@users.noreply.github.com>
2024-01-18 21:00:04 +0800

    Add files via upload

--- auto_transcribe.py
+++ auto_transcribe.py
@@ -1,4 +1,5 @@
 from config import config
+import transcribe_tools
 import json
 import argparse
 import time
@@ -9,7 +10,7 @@ if __name__ == "__main__":
     target_sr = hps['data']['sampling_rate']
     parser = argparse.ArgumentParser()
     parser.add_argument("--engine", default="whisper",type=str)
-    parser.add_argument("--languages", default="CJE",type=str)
+    parser.add_argument("--languages", default="M",type=str)
     parser.add_argument("--whisper_size", default="medium",type=str)
     parser.add_argument("--transcription_path", default=config.preprocess_text_config.transcription_path,type=str)
     parser.add_argument("--in_dir", type=str, default=config.resample_config.in_dir, help="path to source dir")
@@ -17,19 +18,15 @@ if __name__ == "__main__":
     parser.add_argument("--sr", default=target_sr,type=int)
     parser.add_argument("--processes", default=1,type=int)    
     args = parser.parse_args()
-
-    if args.engine=="funasr":
-        from transcribe_tools.funasr_transcribe import run
-        print("[INFO]: Use Funasr to trancribe...")
-    elif args.engine=="genshin":
-        from transcribe_tools.transcribe_genshin import run
-        print("[INFO]: Processing genshin datasets ...")
-    else:
-        from transcribe_tools.whisper_transcribe import run
-        print("[INFO]: Use Whisper to trancribe...")
-        
     t1 = time.time()
-    run(args)
+    transcribe_tools.transcribe(args.engine,
+                                args.languages,
+                                args.whisper_size,
+                                args.transcription_path,
+                                args.in_dir,args.out_dir,
+                                args.sr,args.processes,
+                                False
+                                ).run_transcribe()    
     t2 = time.time()
     m, s = divmod(t2-t1, 60)
     use_time="%02d:%02d"%(m, s)
